be0b00f13551c7450657e9b0b4e95a61357d08d0
David Schall <david.schall@ed.ac.uk>
2023-06-27 08:59:36 +0000

    cpu: Implement decoupled front-end
    This commit comprises most of the changes to add
    the decoupled front-end. Here just the most
    important bits:
    - Creates a new pipeline stage called BAC (Branch and
      Address Calculation).
    - The branch predictor is moved to the new stage
      and with it all the functionality related to it.
      (e.g. squashing is done in BAC)
    - The BAC stage contains two parts the branch
      predictor part and the next PC calculation. Both
      can be decoupled and connected with the fetch
      target queue.
    
    Note: this commit will not commit as the BPU must
    be updated.
    
    Change-Id: Ieb9b6d3cd6cfbdc2408b5e2c341af20650dd1b3c
    Signed-off-by: David Schall <david.schall@ed.ac.uk>

--- src/cpu/o3/ftq.cc
+++ src/cpu/o3/ftq.cc
@@ -2,16 +2,14 @@
  * Copyright (c) 2022-2023 The University of Edinburgh
  * All rights reserved
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met: redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer;
- * redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution;
- * neither the name of the copyright holders nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
+ * The license below extends only to copyright in the software and shall
+ * not be construed as granting a license to any other intellectual
+ * property including but not limited to intellectual property relating
+ * to a hardware implementation of the functionality of the software
+ * licensed hereunder.  You may use the software subject to the license
+ * terms below provided that you ensure that this notice is replicated
+ * unmodified and in its entirety in all distributions of the software,
+ * modified or unmodified, in source code or in binary form.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
